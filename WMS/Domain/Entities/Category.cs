eff17be3f64386d940028946b2b448c7d4bebfa3
Oleksandr-webp <voldfox4@gmail.com>
2026-09-10 11:09:56 +0200

    Added new Category class

---
+++ WMS/Domain/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace WMS.Domain.Entities
+{
+    public class Category
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public bool isActive { get; set; }
+    }
+}
